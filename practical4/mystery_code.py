fb7366e704625842bb6bf4cac304e66c702f12e0
jun <1412502225@qq.com>
2024-03-06 16:05:50 +0800

    edit

---
+++ practical4/mystery_code.py
@@ -0,0 +1,19 @@
+# What does this piece of code do?
+# Answer:Produce 99 random number from 1 to 9, and add them together.
+# Import libraries
+# randint allows drawing a random number,
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+
+progress=0
+total_random_number=0
+while progress<100:
+	progress+=1
+	n = randint(1,10)
+	total_random_number = total_random_number+n
+
+print(total_random_number)
