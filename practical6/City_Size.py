a84c477922864424c813e6201187548240223b62
jun <1412502225@qq.com>
2024-03-19 19:22:15 +0800

    City_Size.py

---
+++ practical6/City_Size.py
@@ -0,0 +1,19 @@
+uk_cities=[0.56,0.62,0.04,9.7]
+uk_name=["Edinburgh","Glasgow","Stirling","London"]
+import matplotlib.pyplot as plt
+plt.figure()
+plt.bar(uk_name,uk_cities)
+plt.title('UK_City_Size')
+plt.xlabel('City')
+plt.ylabel('Population')
+plt.show()
+plt.clf
+plt.figure()
+china_cities=[0.58,8.4,29.9,22.2]
+china_name=['Haining','Hangzhou','Shanghai','Beijing']
+plt.bar(china_name,china_cities)
+plt.title('China_City_size')
+plt.xlabel('City')
+plt.ylabel('Population')
+plt.show()
+plt.clf()
--- practical6/charts.py
+++ practical6/charts.py
@@ -3,4 +3,5 @@ Activity= ["sleeping", "classes", "Studying", "TV", "Music","Others"]
 Time_spent= [8, 6, 3.5, 2, 1,3.5]
 plt.figure()
 plt.pie(Time_spent, labels=Activity, startangle=90)
-plt.show
+plt.title('The average day of a university student')
+plt.show()
